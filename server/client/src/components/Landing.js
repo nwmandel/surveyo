c5f2ec6b2d8049b8a253a8e2c9dbdf19632f8023
Nicholas Mandel <nwmandel@gmail.com>
2019-03-11 16:09:27 -0700

    naming

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Emaily!</h1>
+      <h1>Surveyo!</h1>
       Collect feedback from users
     </div>
   );
