10e9559294d69c99e2f30a377db0f40d9c38ee93
Nicholas Mandel <nwmandel@gmail.com>
2019-03-19 15:27:46 -0700

    api call to send survey to emails

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,18 +1,14 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
-
-const FIELDS = [
-  { label: 'Survey Title', name:'title' },
-  { label: 'Subject Line', name:'subject' },
-  { label: 'Email Body', name:'body' },
-  { label: 'Recipient List', name:'emails' },
-];
+import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields';
 
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(FIELDS, ({ label, name }) => {
+    return _.map(formFields, ({ label, name }) => {
       return (
         <Field key={name} component={SurveyField} type="text" label={label} name={name} />
       );
@@ -22,8 +18,11 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
           <button type="submit" className="teal btn-flat right white-text">
             Next
             <i className="material-icons right">done</i>
@@ -34,6 +33,22 @@ class SurveyForm extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  _.each(formFields, ({ label, name }) => {
+    if (!values[name]) {
+      errors[name] = `You must provide ${label}`;
+    }
+  });
+
+  errors.recipients = validateEmails(values.recipients || '');
+  
+  return errors;
+}
+
 export default reduxForm({
-  form: 'surveyForm'
+  validate,
+  form: 'surveyForm',
+  destroyOnUnmount: false,
 })(SurveyForm);
